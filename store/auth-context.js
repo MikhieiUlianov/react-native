81bb9129f8b1cb118b74114a591b8d0eb14c4281
Mkh <example@gmail.com>
2025-10-12 13:25:00 +0200

    storing-token-on-devices

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext({
@@ -9,12 +10,13 @@ export const AuthContext = createContext({
 
 export const AuthContextProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState();
-
   function authenticate(token) {
     setAuthToken(token);
+    AsyncStorage.setItem("token", token);
   }
   function logout() {
     setAuthToken(null);
+    AsyncStorage.removeItem("token");
   }
 
   const ctx = {
