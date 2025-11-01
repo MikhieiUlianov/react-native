3256b9120c94ecd9b589ad9593e60d1fd991c309
Mkh <example@gmail.com>
2025-11-01 09:46:31 +0100

    image-preview-&-custom-button-component

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,54 +1,84 @@
-import { Button, View, StyleSheet, Alert } from "react-native-web";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
+import { useState } from "react";
 
-const ImagePicker = () => {
-  const [cameraPermissionsInformation, requestPermission] =
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlineButton";
+
+function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
+  const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
-  async function verifyPermisions() {
-    if (cameraPermissionsInformation.status === PermissionStatus.UNDETERMINED) {
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
       const permissionResponse = await requestPermission();
 
       return permissionResponse.granted;
     }
 
-    if (
-      (cameraPermissionsInformation.permissionResponse =
-        PermissionStatus.DENIED)
-    ) {
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
       Alert.alert(
         "Insufficient Permissions!",
         "You need to grant camera permissions to use this app."
       );
-
       return false;
     }
 
     return true;
   }
-  async function ImageHandler() {
-    const hasPermissions = await verifyPermisions();
 
-    if (!hasPermissions) return;
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+
+    setPickedImage(image.assets[0].uri);
   }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
+
   return (
     <View>
-      <View></View>
-      <Button onPress={ImageHandler} title="Take Image" />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton onPress={takeImageHandler} icon={"camera"}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
-};
+}
 
 export default ImagePicker;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
