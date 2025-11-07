c2b7113987b39db3436b911129d9dc597d7246d3
Mkh <example@gmail.com>
2025-11-07 15:29:39 +0100

    outputting-list-&-styling-place-items-&-connecting-sqlite-&-preparing-code-to-insert-data

--- App.js
+++ App.js
@@ -7,10 +7,25 @@ import AddPlaces from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
 import { Colors } from "./constans/colors";
 import Map from "./screens/Map";
+import { useEffect, useState } from "react";
+import { init } from "./util/database";
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (!dbInitialized) return <AppLoading />;
+
   return (
     <>
       <StatusBar style="dark" />
--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,7 +1,7 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 
+import { Colors } from "../../constants/colors";
 import PlaceItem from "./PlaceItem";
-import { Colors } from "../../constans/colors";
 
 function PlacesList({ places }) {
   if (!places || places.length === 0) {
@@ -16,6 +16,7 @@ function PlacesList({ places }) {
 
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
@@ -26,6 +27,9 @@ function PlacesList({ places }) {
 export default PlacesList;
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 24,
+  },
   fallbackContainer: {
     flex: 1,
     justifyContent: "center",
