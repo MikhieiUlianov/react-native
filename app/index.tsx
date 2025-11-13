29ba7fe22c7ae47b4d462eb888505506d179823a
Mkh <example@gmail.com>
2025-11-13 12:30:51 +0100

    configuring-&-handling-incoming-notifications

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,46 @@
-import { Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
+export default function HomeScreen() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first notification!",
+        body: "Notification body",
+        data: { userName: "Name" },
+      },
+      trigger: {
+        type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+        seconds: 2,
+      },
+    });
+  }
+
+  <View style={styles.container}>
+    <Button
+      title="Schedule Notification"
+      onPress={scheduleNotificationHandler}
+    />
+    <StatusBar style="auto" />
+  </View>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
